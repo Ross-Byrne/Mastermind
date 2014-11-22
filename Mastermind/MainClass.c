3166402515c1bd24dbe3cda21bc77de2c14a4007
Ross Byrne <ross.byrne@outlook.ie>
2014-11-22 00:52:29 +0000

    Working Away

--- Mastermind/MainClass.c
+++ Mastermind/MainClass.c
@@ -3,8 +3,12 @@
 
 int main(void)
 {
-	char gameBoard[16][5]; // game board is 15 rows of 4 pegs
-	char feedbackBoard[16][5]; // board for feedback pins, while or black
+	
+	char gameBoard[GBROWS][GBCOLUMNS]; 
+	char feedbackBoard[16][5]; 
+	char gameSolution[5]; 
+	char playersGuess[5]; 
+
 	int endGame = 0, menuChoice = 0;
 
 	printf("Welcome To Mastermind.");
@@ -13,15 +17,14 @@ int main(void)
 	while(endGame != 99)
 	{
 		// To make sure the number input is in the right range
-		// and that it is actually an int
 		do
 		{
-			printGameMenu();
+			printMainGameMenu();
 
 			printf("\n\nEnter Option: ");
 		
 			fflush(stdin); // flush buffer
-			scanf("%d",&menuChoice);
+			scanf_s("%d",&menuChoice);
 
 		}while((menuChoice < 1) || (menuChoice > 2));
 
@@ -29,7 +32,7 @@ int main(void)
 		{
 		case 1:
 			printf("Play Game!");
-			//endGame = 0;
+			playGame(gameBoard);
 			break;
 		case 2:
 			printf("Exiting");
@@ -43,10 +46,60 @@ int main(void)
 	system("pause");
 } // main()
  
-void printGameMenu()
+void printMainGameMenu()
 {
-	// prints game menu
-	printf("\n\n\Game Menu\n");
+	printf("\n\nMain Game Menu\n");
 	printf("\n1.) Start Playing Mastermind.");
 	printf("\n2.) Exit.");
 } // printGameMenu()
+
+void printGameMenu()
+{
+	printf("\n\nGame Menu\n");
+	printf("\n1.) Make A Guess.");
+	printf("\n2.) Exit.");
+} // printGameMenu()
+
+void playGame(char gameBoard[GBROWS][GBCOLUMNS])
+{
+	int menuChoice = 0;
+
+	printf("\nThe aim of the game is to guess the colour and position of four pins.");
+	printf("\nThe pin colours are all the colours of the rainbow:");
+	printf("\nRed, Orange, Yellow, Green, Blue, Indigo and Violet.\n");
+
+	while(menuChoice != 99)
+	{
+
+		// To make sure the number input is in the right range
+		do
+		{
+			printGameMenu();
+
+			printf("\n\nEnter Option: ");
+		
+			fflush(stdin); // flush buffer
+			scanf_s("%d",&menuChoice);
+
+		}while((menuChoice < 1) || (menuChoice > 2));
+
+		switch(menuChoice)
+		{
+		case 1:
+			printf("\nMake A Guess\n");
+
+
+			//endGame = 0;
+			break;
+		case 2:
+			printf("Exiting");
+			menuChoice = 99;
+			break;
+		} // switch
+
+
+
+	
+	} // while
+
+} // playGame()
--- Mastermind/MainClass.h
+++ Mastermind/MainClass.h
@@ -3,5 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GBROWS 15
+#define GBCOLUMNS 5
+
 // Fuctions
+void printMainGameMenu();
 void printGameMenu();
+void playGame(char gameBoard[GBROWS][GBCOLUMNS]);
